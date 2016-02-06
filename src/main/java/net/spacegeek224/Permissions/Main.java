ee8018245e7b1e6a9cc31e960adb0c340842807d
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-02-06 10:00:26 -0500

    Update Main.java

--- src/main/java/net/spacegeek224/Permissions/Main.java
+++ src/main/java/net/spacegeek224/Permissions/Main.java
@@ -2,6 +2,7 @@ package net.spacegeek224.Permissions;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
