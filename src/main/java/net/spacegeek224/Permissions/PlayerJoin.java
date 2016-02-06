a42ca52b7e3fea2fd6f667ba5259070f1711e3a8
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-02-06 09:47:16 -0500

    Update PlayerJoin.java

--- src/main/java/net/spacegeek224/Permissions/PlayerJoin.java
+++ src/main/java/net/spacegeek224/Permissions/PlayerJoin.java
@@ -7,7 +7,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -16,41 +16,9 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-public class PlayerChat implements Listener {
+public class PlayerJoin implements Listener {
   @EventHandler
-  public void onPlayerChat(AsyncPlayerChatEvent event) {
-    String msg = event.getMessage();
+  public void onPlayerJoin(PlayerJoinEvent event) {
     Player player = event.getPlayer();
-    String[] swearWords = {"fuck","ass","bitch"};
-    String message = event.getMessage().toLowerCase().replaceAll("@", "a").replaceAll("\\p{Punct}", " ");
-    for(String word : swearWords){
-      if(message.matches("(.* )?"+word+"( .*)?")) {
-        // There has been a swear word in the message
-        msg = msg.replaceAll(word, toBleep(word));
-      } else {
-        // The current swear word was not found in the message.
-      }
-    }
-   event.setCancelled(true);
-   if (player.hasPermission("rank.admin")) {
-    Bukkit.broadcastMessage(ChatColor.RED + player.getDisplayName() + ": " + ChatColor.WHITE + msg);
-   } else if (player.hasPermission("rank.mod")) {
-    Bukkit.broadcastMessage(ChatColor.GREEN + player.getDisplayName() + ": " + ChatColor.WHITE + msg);
-   } else if (player.hasPermission("rank.helper")) {
-    Bukkit.broadcastMessage(ChatColor.BLUE + player.getDisplayName() + ": " + ChatColor.WHITE + msg);
-   } else if (player.hasPermission("rank.vip")) {
-    Bukkit.broadcastMessage(ChatColor.GREEN + player.getDisplayName() + ": " + ChatColor.WHITE + msg);
-   } else {
-     Bukkit.broadcastMessage(ChatColor.GRAY + player.getDisplayName() + ": " + ChatColor.WHITE + msg);
-   }
-   
-  }
-  public String toBleep(String s) {
-    int len = s.length();
-    StringBuilder sb = new StringBuilder(len);
-    for(int i = 0; i < len; i++){
-      sb.append('*');
-    }
-    return sb.toString();
   }
 }
