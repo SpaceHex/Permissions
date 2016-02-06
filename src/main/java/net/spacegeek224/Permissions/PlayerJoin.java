7d9174384dab0d7dbc9b091a4073f2fb1e27e138
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-02-06 10:09:16 -0500

    Update PlayerJoin.java

--- src/main/java/net/spacegeek224/Permissions/PlayerJoin.java
+++ src/main/java/net/spacegeek224/Permissions/PlayerJoin.java
@@ -10,11 +10,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.permissions.PermissionAttachment;
 
 public class PlayerJoin implements Listener {
   @EventHandler
