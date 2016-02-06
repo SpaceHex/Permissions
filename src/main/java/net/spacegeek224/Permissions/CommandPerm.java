ea24157849ea84d554f705b1ba94bf08c221a38a
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-02-06 11:22:34 -0500

    Update CommandPerm.java

--- src/main/java/net/spacegeek224/Permissions/CommandPerm.java
+++ src/main/java/net/spacegeek224/Permissions/CommandPerm.java
@@ -25,9 +25,9 @@ public class CommandPerm implements CommandExecutor {
         }          
         Player getPlayerByName(String playerName) {
         Player playerToReturn = null;
-        for(int i = 0; i < getServer().getOnlinePlayers().length; i++) {
-            if(getServer().getOnlinePlayers()[i].getName().equalsIgnoreCase(playerName)) {
-                playerToReturn = getServer().getOnlinePlayers()[i];
+        for(int i = 0; i < Bukkit.getServer().getOnlinePlayers().length; i++) {
+            if(Bukkit.getServer().getOnlinePlayers()[i].getName().equalsIgnoreCase(playerName)) {
+                playerToReturn = Bukkit.getServer().getOnlinePlayers()[i];
                 break;
             }
         }
@@ -36,9 +36,9 @@ public class CommandPerm implements CommandExecutor {
  
     OfflinePlayer getOfflinePlayerByName(String playerName) {
         OfflinePlayer playerToReturn = null;
-        for(int i = 0; i < getServer().getOfflinePlayers().length; i++) {
-            if(getServer().getOfflinePlayers()[i].getName().equalsIgnoreCase(playerName)) {
-                playerToReturn = getServer().getOfflinePlayers()[i];
+        for(int i = 0; i < Bukkit.getServer().getOfflinePlayers().length; i++) {
+            if(Bukkit.getServer().getOfflinePlayers()[i].getName().equalsIgnoreCase(playerName)) {
+                playerToReturn = Bukkit.getServer().getOfflinePlayers()[i];
                 break;
             }
         }
