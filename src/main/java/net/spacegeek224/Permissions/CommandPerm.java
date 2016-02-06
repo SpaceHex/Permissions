0bfa39f61a6c4623e4518707751e47f8de4dce08
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-02-06 10:40:46 -0500

    Update CommandPerm.java

--- src/main/java/net/spacegeek224/Permissions/CommandPerm.java
+++ src/main/java/net/spacegeek224/Permissions/CommandPerm.java
@@ -12,7 +12,19 @@ public class CommandPerm implements CommandExecutor {
         public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {                       
                 
                 //Player player = (Player) sender;
-                
+                if (args.length == 0) {
+                        
+                } else if (args.length == 4) {
+                        if (args[0] == "user") {
+                                if (args[2] == "add") {
+                                        for (Player on : Bukkit.getOnlinePlayers()) {
+                                                if (on.getDisplayName()==args[1]){
+                                                        on.sendMessage("hi");
+                                                }
+                                        }
+                                }
+                        }
+                }
                 return true;                                                                                            
         }                                                                                                                
                                                                                                                          
