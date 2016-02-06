fa864081a2b916765614b9cbcec36899ca238504
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-02-06 10:30:30 -0500

    Create CommandPerm.java

---
+++ src/main/java/net/spacegeek224/Permissions/CommandPerm.java
@@ -0,0 +1,19 @@
+package net.spacegeek224.Permissions;                                                                                     
+                                                                                                                         
+import org.bukkit.Bukkit;                                                                                                
+import org.bukkit.ChatColor;                                                                                             
+import org.bukkit.command.Command;                                                                                       
+import org.bukkit.command.CommandExecutor;                                                                               
+import org.bukkit.command.CommandSender;                                                                                
+import org.bukkit.entity.Player;                                                                                         
+                                                                                                                         
+public class CommandPerm implements CommandExecutor {                                                                    
+        @Override                                                                                                        
+        public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {                       
+                
+                //Player player = (Player) sender;
+                
+                return true;                                                                                            
+        }                                                                                                                
+                                                                                                                         
+}  
